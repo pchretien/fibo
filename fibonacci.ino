f20a0de24c20d7bda638a996754a7ae9959be8d3
Philippe Chretien <philippe.chretien@gmail.com>
2016-04-14 11:14:48 -0400

    Type in comments

--- fibonacci.ino
+++ fibonacci.ino
@@ -1,6 +1,6 @@
 // Fibonacci Clock
 //
-/ This program is free software; you can redistribute it and/or
+// This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License Version 2
 //
 // This program is distributed in the hope that it will be useful,
